1bc893325d6fec2807ba7573fe14d60cccd2d3c1
andresalinas18 <afsalinas18@gmail.com>
2025-06-03 16:43:32 -0500

    feat: close the toggle menu when clicked

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+/* toogle menu */
 const mobileMenu = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links a');
 
 mobileMenu.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+});
+
+ /*Close the toggle menu when clicked */
+navItems.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
 });
